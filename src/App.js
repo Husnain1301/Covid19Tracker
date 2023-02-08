01c92887ebb2e66c6fb0a8013d7e632a7b6f6465
Bajwa <husnain.bajwa@capgemini.com>
2023-02-08 03:01:39 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@mui/material"
+import { useEffect, useState } from 'react';
+
+// USE EFFECT = runs a piece of code based on a given condition
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  //STATE = HOW TO WRITE A VARIABLE IN REACT <<<<<<<
+
+  useEffect(() => {
+    // send a request to the server.. then WAIT... then do something with the info
+
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries").then((response) => response.json()).then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country, // E.g United Kingdom
+            value: country.countryInfo.iso2 // E.g UK
+          }));
+        setCountries(countries);
+      });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='app__header'>
+        <h1>COVID 19 Tracker</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' value="abc">
+            {countries.map((country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            )))}
+            {/* 
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+  <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+      
+
+      {/*HEADER*/}
+      {/*TITLE + SELECT INPUT DROPDOWN*/}
+
+      {/*INFOBOXES*/}
+      {/*INFOBOXES*/}
+      {/*INFOBOXES*/}
+
+      {/*TABLE*/}
+
+      {/*GRAPH*/}
+
+      {/*MAP*/}
     </div>
   );
 }
